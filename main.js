3b377e76e6c417a222f25a86719e4861ea1bcc6f
AlineNunes2023 <alinenunespp@gmail.com>
2023-05-28 11:38:05 -0300

    Versão Final

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-//getelementbyid
-const pegavalorDaConta = document.getElementById("valorConta");
+function sendForm(){
+/* const pegaValorDaConta = document.getElementById("valorConta");
+const pegaValorDaTaxa = document.getElementById("ValorTaxa");
+const pegaQuantidadedePessoas = document.getElementById(quantidadedePessoas);
 
-function exibirValorDaConta() {
-console.log(pegavalorDaConta.value);
+if(pegaValorDaConta.value ==""){
+alert("Insira o valor da Conta");
+}else if(pegaValorDaTaxa.value =="") {
+alert("Insira o Valor da Taxa");
+}else if(pegaQuantidadedePessoas.value == "") {
+alert("Insira a quantidade de Pessoas");
+}else {
+    formEvent()
+} */
+}
 
+function calcularConta() {
+    const pegaValorDaConta = document.getElementById("valorConta");
+    const pegaValorDaTaxa = document.getElementById("ValorTaxa");
+    const pegaQuantidadedePessoas = document.getElementById("quantidadedePessoas");
+const calcValorFinal = parseInt(pegaValorDaConta.value) + parseInt(pegaValorDaTaxa.value) / parseInt(pegaQuantidadedePessoas.value);
+
+document.getElementById("resultado").innerHTML = calcValorFinal;
+}
+
+function formEvent(){
+const form = document.getElementById("form");
+form.addEventListener("submit", (e)=> {
+e.preventDefault();
+
+calcularConta();
+});
 
 }
-pegavalorDaConta.addEventListener("change", exibirValorDaConta)
-//addEventListener
-
-
